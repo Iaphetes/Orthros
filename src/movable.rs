ece50243bd1d2116b2f60d9e1d97f15a4793d305
Adrian Osterwind <adrian_osterwind@zoho.eu>
2022-11-02 20:24:14 +0100

    fixed devide by 0 in move_units

--- src/movable.rs
+++ src/movable.rs
@@ -12,14 +12,16 @@ pub fn move_units(
     mut commands: Commands,
 ) {
     for (mut entity, mut transform, movetarget) in movable_units.iter_mut() {
-        let rotation_xz: f32 = Vec2 {
-            x: movetarget.target.x - transform.translation.x,
-            y: movetarget.target.z - transform.translation.z,
+        if movetarget.target != transform.translation {
+            let rotation_xz: f32 = Vec2 {
+                x: movetarget.target.x - transform.translation.x,
+                y: movetarget.target.z - transform.translation.z,
+            }
+            .angle_between(Vec2 { x: 0.0, y: 1.0 });
+            println!("{:?}", rotation_xz);
+            transform.rotation = Quat::from_rotation_y(rotation_xz);
+            transform.translation = movetarget.target;
         }
-        .angle_between(Vec2 { x: 0.0, y: 1.0 });
-        println!("{:?}", rotation_xz);
-        transform.rotation = Quat::from_rotation_y(rotation_xz);
-        transform.translation = movetarget.target;
         commands.entity(entity).remove::<MoveTarget>();
     }
 }

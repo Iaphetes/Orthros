48247d4a4f1289cc0fe155fb4c6f29b12060d6cc
Adrian Osterwind <adrian_osterwind@zoho.eu>
2022-12-08 07:28:35 +0100

    started porting astar to main game

--- src/movable.rs
+++ src/movable.rs
@@ -2,11 +2,55 @@ use bevy::ecs::component::Component;
 use bevy::math::Vec3;
 use bevy::prelude::*;
 use bevy::transform::components::Transform;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
 #[derive(Component)]
 pub struct MoveTarget {
     pub target: Vec3,
 }
+const DISTANCE_FACTOR: f32 = 100.0;
 
+struct NodeCoords {
+    xy: UVec2,
+    h: Heading,
+}
+
+#[derive(Eq, PartialEq, Hash, Clone, EnumIter, Debug)]
+enum Heading {
+    N,
+    NE,
+    E,
+    SE,
+    S,
+    SW,
+    W,
+    NW,
+}
+
+#[derive(Component)]
+struct MovementGrid {
+    grid: Vec<Vec<u8>>,
+}
+#[derive(Component)]
+struct MoveCommand {
+    target: Vec2,
+    path: Vec<NodeCoords>,
+}
+#[derive(Component)]
+struct Movable {}
+
+#[derive(Resource)]
+struct GridSettings {
+    cell_size: f32,
+    grid_width: u32,
+    grid_height: u32,
+    x_y_offset: Vec2,
+    density: f64, // TODO put into map generation
+}
+
+#[derive(Resource)]
+struct MovementTimer(Timer);
 pub fn move_units(
     mut movable_units: Query<(Entity, &mut Transform, &MoveTarget)>,
     mut commands: Commands,
